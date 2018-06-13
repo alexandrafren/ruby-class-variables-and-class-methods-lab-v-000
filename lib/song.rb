22f77a5748862285308e77619ec9a4d3d80981d0
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-13 15:12:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
       if genre_hash.include?(genre)
         genre_hash[genre] += 1
       else
-        genre_hash = genre_hash
+        genre_hash[genre]
         genre_hash[genre] += 1
       end
     end
