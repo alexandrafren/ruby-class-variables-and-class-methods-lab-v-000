7aa786d0bdc0a00968af4186f614a99536dac082
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-13 15:34:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,10 +41,10 @@ class Song
     genre_hash = {}
     @@genres.each do |genre|
       if genre_hash.include?(genre)
-        genre_hash[genre] += 1
+        genre_hash[:genre] += 1
       else
-        genre_hash[genre]
-        genre_hash[genre] += 1
+        genre_hash[:genre]
+        genre_hash[:genre] += 1
       end
     end
   end
